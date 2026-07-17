287d5e36169c92d8071ab5fd258904c65478f11c
minjiemei <minjie.mei@beesoft.io>
2026-07-17 10:27:39 +0800

    Add the required styles

--- Example/MailExample/MailTableViewController.swift
+++ Example/MailExample/MailTableViewController.swift
@@ -5,45 +5,45 @@
 //  Copyright © 2017 Jeremy Koch. All rights reserved.
 //
 
-import UIKit
 import SwipeCellKit
+import UIKit
 
 class MailTableViewController: UITableViewController {
     var emails: [Email] = []
-    
+
     var defaultOptions = SwipeOptions()
     var isSwipeRightEnabled = true
-    var buttonDisplayMode: ButtonDisplayMode = .titleAndImage
+    var buttonDisplayMode: ButtonDisplayMode = .imageOnly
     var buttonStyle: ButtonStyle = .backgroundColor
     var usesTallCells = false
-    
+
     // MARK: - Lifecycle
-    
+
     override func viewDidLoad() {
         tableView.allowsSelection = true
         tableView.allowsMultipleSelectionDuringEditing = true
-        
+
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 100
-        
+
         navigationItem.rightBarButtonItem = editButtonItem
-        
+
         view.layoutMargins.left = 32
-        
+
         resetData()
     }
-    
+
     // MARK: - Table view data source
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return emails.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MailCell") as! MailCell
         cell.delegate = self
         cell.selectedBackgroundView = createSelectedBackgroundView()
-        
+
         let email = emails[indexPath.row]
         cell.fromLabel.text = email.from
         cell.dateLabel.text = email.relativeDateString
@@ -51,26 +51,26 @@ class MailTableViewController: UITableViewController {
         cell.bodyLabel.text = email.body
         cell.bodyLabel.numberOfLines = usesTallCells ? 0 : 2
         cell.unread = email.unread
-        
+
         return cell
     }
-    
+
     func visibleRect(for tableView: UITableView) -> CGRect? {
         if usesTallCells == false { return nil }
-        
+
         if #available(iOS 11.0, *) {
             return tableView.safeAreaLayoutGuide.layoutFrame
         } else {
             let topInset = navigationController?.navigationBar.frame.height ?? 0
             let bottomInset = navigationController?.toolbar?.frame.height ?? 0
             let bounds = tableView.bounds
-            
+
             return CGRect(x: bounds.origin.x, y: bounds.origin.y + topInset, width: bounds.width, height: bounds.height - bottomInset)
         }
     }
-    
+
     // MARK: - Actions
-    
+
     @IBAction func moreTapped(_ sender: Any) {
         let controller = UIAlertController(title: "Swipe Transition Style", message: nil, preferredStyle: .actionSheet)
         controller.addAction(UIAlertAction(title: "Border", style: .default, handler: { _ in self.defaultOptions.transitionStyle = .border }))
@@ -84,7 +84,7 @@ class MailTableViewController: UITableViewController {
         controller.addAction(UIAlertAction(title: "Reset", style: .destructive, handler: { _ in self.resetData() }))
         present(controller, animated: true, completion: nil)
     }
-    
+
     func buttonDisplayModeTapped() {
         let controller = UIAlertController(title: "Button Display Mode", message: nil, preferredStyle: .actionSheet)
         controller.addAction(UIAlertAction(title: "Image + Title", style: .default, handler: { _ in self.buttonDisplayMode = .titleAndImage }))
@@ -93,7 +93,7 @@ class MailTableViewController: UITableViewController {
         controller.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         present(controller, animated: true, completion: nil)
     }
-    
+
     func buttonStyleTapped() {
         let controller = UIAlertController(title: "Button Style", message: nil, preferredStyle: .actionSheet)
         controller.addAction(UIAlertAction(title: "Background Color", style: .default, handler: { _ in
@@ -107,7 +107,7 @@ class MailTableViewController: UITableViewController {
         controller.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         present(controller, animated: true, completion: nil)
     }
-    
+
     func cellHeightTapped() {
         let controller = UIAlertController(title: "Cell Height", message: nil, preferredStyle: .actionSheet)
         controller.addAction(UIAlertAction(title: "Normal", style: .default, handler: { _ in
@@ -121,15 +121,15 @@ class MailTableViewController: UITableViewController {
         controller.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         present(controller, animated: true, completion: nil)
     }
-    
+
     // MARK: - Helpers
-    
+
     func createSelectedBackgroundView() -> UIView {
         let view = UIView()
         view.backgroundColor = UIColor.lightGray.withAlphaComponent(0.2)
         return view
     }
-    
+
     func resetData() {
         emails = mockEmails
         emails.forEach { $0.unread = false }
@@ -141,38 +141,38 @@ class MailTableViewController: UITableViewController {
 extension MailTableViewController: SwipeTableViewCellDelegate {
     func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
         let email = emails[indexPath.row]
-        
+
         if orientation == .left {
             guard isSwipeRightEnabled else { return nil }
-            
-            let read = SwipeAction(style: .default, title: nil) { action, indexPath in
+
+            let read = SwipeAction(style: .default, title: nil) { _, indexPath in
                 let updatedStatus = !email.unread
                 email.unread = updatedStatus
-                
+
                 let cell = tableView.cellForRow(at: indexPath) as! MailCell
                 cell.setUnread(updatedStatus, animated: true)
             }
-            
+
             read.hidesWhenSelected = true
             read.accessibilityLabel = email.unread ? "Mark as Read" : "Mark as Unread"
-            
+
             let descriptor: ActionDescriptor = email.unread ? .read : .unread
             configure(action: read, with: descriptor)
-            
+
             return [read]
         } else {
-            let flag = SwipeAction(style: .default, title: nil, handler: nil)
-            flag.hidesWhenSelected = true
-            configure(action: flag, with: .flag)
-            
-            let delete = SwipeAction(style: .destructive, title: nil) { action, indexPath in
-                self.emails.remove(at: indexPath.row)
-            }
-            configure(action: delete, with: .trash)
-            
+//            let flag = SwipeAction(style: .default, title: nil, handler: nil)
+//            flag.hidesWhenSelected = true
+//            configure(action: flag, with: .flag)
+//
+//            let delete = SwipeAction(style: .destructive, title: nil) { action, indexPath in
+//                self.emails.remove(at: indexPath.row)
+//            }
+//            configure(action: delete, with: .trash)
+
             let cell = tableView.cellForRow(at: indexPath) as! MailCell
             let closure: (UIAlertAction) -> Void = { _ in cell.hideSwipe(animated: true) }
-            let more = SwipeAction(style: .default, title: nil) { action, indexPath in
+            let more = SwipeAction(style: .default, title: nil) { _, _ in
                 let controller = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
                 controller.addAction(UIAlertAction(title: "Reply", style: .default, handler: closure))
                 controller.addAction(UIAlertAction(title: "Forward", style: .default, handler: closure))
@@ -183,39 +183,45 @@ extension MailTableViewController: SwipeTableViewCellDelegate {
                 self.present(controller, animated: true, completion: nil)
             }
             configure(action: more, with: .more)
-            
-            return [delete, flag, more]
+            more.cornerRadius = 12
+            more.actionBackgroundColor = .gray
+            more.backgroundColor = .clear
+
+            return [more]
         }
     }
-    
+
     func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
         var options = SwipeOptions()
-        options.expansionStyle = orientation == .left ? .selection : .destructive
-        options.transitionStyle = defaultOptions.transitionStyle
-        
-        switch buttonStyle {
-        case .backgroundColor:
-            options.buttonSpacing = 4
-        case .circular:
-            options.buttonSpacing = 4
-        #if canImport(Combine)
-            if #available(iOS 13.0, *) {
-                options.backgroundColor = UIColor.systemGray6
-            } else {
-                options.backgroundColor = #colorLiteral(red: 0.9467939734, green: 0.9468161464, blue: 0.9468042254, alpha: 1)
-            }
-        #else
-            options.backgroundColor = #colorLiteral(red: 0.9467939734, green: 0.9468161464, blue: 0.9468042254, alpha: 1)
-        #endif
-        }
-        
+        options.buttonSpacing = 8
+        options.topSpacing = 8
+        options.backgroundColor = .white
+//        options.expansionStyle = orientation == .left ? .selection : .destructive
+//        options.transitionStyle = defaultOptions.transitionStyle
+//
+//        switch buttonStyle {
+//        case .backgroundColor:
+//            options.buttonSpacing = 4
+//        case .circular:
+//            options.buttonSpacing = 4
+//        #if canImport(Combine)
+//            if #available(iOS 13.0, *) {
+//                options.backgroundColor = UIColor.systemGray6
+//            } else {
+//                options.backgroundColor = #colorLiteral(red: 0.9467939734, green: 0.9468161464, blue: 0.9468042254, alpha: 1)
+//            }
+//        #else
+//            options.backgroundColor = #colorLiteral(red: 0.9467939734, green: 0.9468161464, blue: 0.9468042254, alpha: 1)
+//        #endif
+//        }
+
         return options
     }
-    
+
     func configure(action: SwipeAction, with descriptor: ActionDescriptor) {
         action.title = descriptor.title(forDisplayMode: buttonDisplayMode)
         action.image = descriptor.image(forStyle: buttonStyle, displayMode: buttonDisplayMode)
-        
+
         switch buttonStyle {
         case .backgroundColor:
             action.backgroundColor = descriptor.color(forStyle: buttonStyle)
@@ -233,47 +239,47 @@ class MailCell: SwipeTableViewCell {
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var subjectLabel: UILabel!
     @IBOutlet var bodyLabel: UILabel!
-    
+
     var animator: Any?
-    
+
     var indicatorView = IndicatorView(frame: .zero)
-    
+
     var unread = false {
         didSet {
-            indicatorView.transform = unread ? CGAffineTransform.identity : CGAffineTransform.init(scaleX: 0.001, y: 0.001)
+            indicatorView.transform = unread ? CGAffineTransform.identity : CGAffineTransform(scaleX: 0.001, y: 0.001)
         }
     }
-    
+
     override func awakeFromNib() {
         setupIndicatorView()
     }
-    
+
     func setupIndicatorView() {
         indicatorView.translatesAutoresizingMaskIntoConstraints = false
         indicatorView.color = tintColor
         indicatorView.backgroundColor = .clear
         contentView.addSubview(indicatorView)
-        
+
         let size: CGFloat = 12
         indicatorView.widthAnchor.constraint(equalToConstant: size).isActive = true
         indicatorView.heightAnchor.constraint(equalTo: indicatorView.widthAnchor).isActive = true
         indicatorView.centerXAnchor.constraint(equalTo: fromLabel.leftAnchor, constant: -16).isActive = true
         indicatorView.centerYAnchor.constraint(equalTo: fromLabel.centerYAnchor).isActive = true
     }
-    
+
     func setUnread(_ unread: Bool, animated: Bool) {
         let closure = {
             self.unread = unread
         }
-        
+
         if #available(iOS 10, *), animated {
             var localAnimator = self.animator as? UIViewPropertyAnimator
             localAnimator?.stopAnimation(true)
-            
+
             localAnimator = unread ? UIViewPropertyAnimator(duration: 1.0, dampingRatio: 0.4) : UIViewPropertyAnimator(duration: 0.3, dampingRatio: 1.0)
             localAnimator?.addAnimations(closure)
             localAnimator?.startAnimation()
-            
+
             self.animator = localAnimator
         } else {
             closure()

00e475a1bb264aae5ca8f1327d72438e8588b179
minjiemei <minjie.mei@beesoft.io>
2026-07-20 13:52:43 +0800

    添加action之间的间隔

--- Example/MailExample/MailTableViewController.swift
+++ Example/MailExample/MailTableViewController.swift
@@ -161,9 +161,12 @@ extension MailTableViewController: SwipeTableViewCellDelegate {
 
             return [read]
         } else {
-//            let flag = SwipeAction(style: .default, title: nil, handler: nil)
-//            flag.hidesWhenSelected = true
-//            configure(action: flag, with: .flag)
+            let flag = SwipeAction(style: .default, title: nil, handler: nil)
+            flag.hidesWhenSelected = true
+            configure(action: flag, with: .flag)
+            flag.cornerRadius = 12
+            flag.actionBackgroundColor = .red
+            flag.backgroundColor = .clear
 //
 //            let delete = SwipeAction(style: .destructive, title: nil) { action, indexPath in
 //                self.emails.remove(at: indexPath.row)
@@ -187,7 +190,7 @@ extension MailTableViewController: SwipeTableViewCellDelegate {
             more.actionBackgroundColor = .gray
             more.backgroundColor = .clear
 
-            return [more]
+            return [more, flag]
         }
     }
 
@@ -196,6 +199,8 @@ extension MailTableViewController: SwipeTableViewCellDelegate {
         options.buttonSpacing = 8
         options.topSpacing = 8
         options.backgroundColor = .white
+        options.minimumButtonWidth = 88 + 12
+        options.rightSpacing = 12
 //        options.expansionStyle = orientation == .left ? .selection : .destructive
 //        options.transitionStyle = defaultOptions.transitionStyle
 //
